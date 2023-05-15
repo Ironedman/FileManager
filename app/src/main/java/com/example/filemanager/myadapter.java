616d0f2e8479f39f3a3bd1629e52f9e925850988
dwiky A_H <98646702+Ironedman@users.noreply.github.com>
2023-05-15 22:23:41 +0700

    comit kedua arduino

--- app/src/main/java/com/example/filemanager/myadapter.java
+++ app/src/main/java/com/example/filemanager/myadapter.java
@@ -1,8 +1,11 @@
 package com.example.filemanager;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,23 +25,37 @@ public class myadapter extends RecyclerView.Adapter<myadapter.Viewholder>{
 
     @Override
     public Viewholder onCreateViewHolder( ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(context).inflate(R.layout.recycler_item,parent,false);
+        return new Viewholder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+        File selectedFile = filesAndFolders[position];
+        holder.textView.setText(selectedFile.getName());
 
+        if (selectedFile.isDirectory()){
+            holder.imageView.setImageResource(R.drawable.baseline_folder_24);
+        }else{
+            holder.imageView.setImageResource(R.drawable.baseline_insert_drive_file_24);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return filesAndFolders.length;
     }
 
     public class Viewholder extends RecyclerView.ViewHolder{
 
+        TextView textView;
+        ImageView imageView;
+
         public Viewholder(View itemView) {
             super(itemView);
+            textView= itemView.findViewById(R.id.text_view);
+            imageView= itemView.findViewById(R.id.icon_view);
         }
     }
 }
